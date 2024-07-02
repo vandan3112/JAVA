e2345721435c4a5dcff770a98f23f5f50a92fa2d
vandan3112 <147312675+vandan3112@users.noreply.github.com>
2024-07-02 10:15:12 +0530

    Add files via upload

---
+++ BankingSynchronization/src/synchronization/AccountThread.java
@@ -0,0 +1,25 @@
+package synchronization;
+
+public class AccountThread extends Thread{
+
+	private Account acc;
+	private int amt;
+	
+	public AccountThread(Account acc,int amt)
+	{
+		this.acc=acc;
+		this.amt=amt;
+		start();
+		
+	}
+	@Override
+	public void run()
+	{	
+		
+		try {
+			acc.withdraw(amt);
+		} catch (InsufficientBalanceException e) {
+			System.err.println(e.getMessage());
+		}
+	}
+}
